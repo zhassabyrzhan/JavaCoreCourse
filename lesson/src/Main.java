10857ef6ab0c5388de091c1d750a00eab98b5135
sabyrka <zhassabyrzhan@gmail.com>
2025-02-11 22:20:01 +0500

    Создан класс Main

---
+++ lesson/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is my first Java program!");
+        System.out.println("I'm learning how to use GitHub!");
+    }
+}
